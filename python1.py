0b2b0567a082e9ef7417373506753710302d3b61
jathinchow <jathin.yarra@gmail.com>
2024-09-30 01:01:31 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,1 +1,10 @@
+with open("chr1_GL383518v1_alt.fa", "r") as file:
+    # Skip the first line (header)
+    file.readline()
+    
+    # Read the rest of the file, which contains the sequence
+    sequence = file.read().replace("\n", "")  # Remove newline characters
 
+# Print the 10th and 758th letters
+print(f"The 10th letter is: {sequence[9]}")
+print(f"The 758th letter is: {sequence[757]}")
